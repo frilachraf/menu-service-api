e574579d97ec600bd9a7220599ccbc50942a2e24
fril achraf <frilachraf.dev@gmail.com>
2025-09-04 00:14:48 +0100

    erroring

---
+++ routes/dishes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+// const router = express.Router();
+
+// router.get('/', (req,res)=>{
+//     console.log(req.query)
+//     res.json(dishes)
+// })
+// router.get('/:id',(req,res)=>{
+//     console.log(req.params);
+//     res.json(dishes[0])
+// })
+
+
+// export default router;
--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const path = require('path');
 const { dishes } = require("./data");
-const app = express();
+// const dishesRoutes = require('./routes/dishes')
 
+const app = express();
 const Port = process.env.PORT || 8000;
 // setup static folder
 
 // app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/api/dishes', (req,res)=>{
-    console.log(req.query)
-    res.json(dishes)
-})
-app.get('/api/dishes/:id',(req,res)=>{
-    console.log(req.params);
-    res.json(dishes[0])
-})
+// routes
+// app.use('/api/dishes',dishesRoutes)
+
+
 app.listen(Port,()=>{
     console.log('server is running on port '+Port);
 })
