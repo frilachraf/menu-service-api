b44e11599605dc42093561ef10ea1b9e177ff84c
fril achraf <frilachraf.dev@gmail.com>
2025-09-03 15:18:10 +0100

    json data

---
+++ data.js
@@ -0,0 +1,13 @@
+export const dishes = [{
+    id: 1, name: 'Pasta', price: 12.99,
+    description: 'Delicious homemade pasta with tomato sauce'
+}, {
+    id: 2, name: 'Pizza', price: 15.99,
+    description: 'Wood-fired pizza with fresh mozzarella and basil'
+}, {
+    id: 3, name: 'Salad', price: 9.99,
+    description: 'Fresh garden salad with a variety of vegetables'
+},
+{    id: 4, name: 'Burger', price: 11.99,
+    description: 'Juicy beef burger with lettuce, tomato, and cheese'
+}];     
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const path = require('path')
+const path = require('path');
+const { dishes } = require("./data");
 const app = express();
 
 const Port = process.env.PORT || 8000;
 // setup static folder
 
-app.use(express.static(path.join(__dirname,'public')))
+// app.use(express.static(path.join(__dirname,'public')))
 
+app.get('/api/dishes', (req,res)=>{
+    // res.send(dishes)
+    res.json(dishes)
+})
 app.listen(Port,()=>{
     console.log('server is running on port '+Port);
 })
+
