c356236aef4dea2e6e4a31b883c4fa119c5479c1
fril achraf <frilachraf.dev@gmail.com>
2025-09-03 15:04:15 +0100

    sendFile

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
-
+const path = require('path')
 const app = express();
 
 const Port = process.env.PORT || 8000;
 
 app.get('/',(req,res)=>{
-    res.send('hello world from express and node.js and mongo db with mongoose and monogodb atl')
+    res.send({message: 'hello message changed'});
+})
+
+app.get('/about',(req,res)=>{
+    // res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(path.join(__dirname ,'public', 'index.html'))
 })
 app.listen(Port,()=>{
     console.log('server is running on port '+Port);
